f2f116072da1e311ce462d525a731eea444766ab
Savio Gabriel Santos <santos.savio@adventistas.org>
2025-02-10 17:59:00 -0300

    Update gerador_pptx.py
    2.0

--- gerador_pptx.py
+++ gerador_pptx.py
@@ -2,21 +2,17 @@ import tkinter as tk
 from tkinter import filedialog
 from pptx import Presentation
 from pptx.util import Inches, Pt
+from pptx.dml.color import RGBColor
+from pptx.enum.text import MSO_ANCHOR, PP_ALIGN
 
-# Defina a resolução da tela
-largura_tela = 1920 
-altura_tela = 1080
-
-# Defina a posição e tamanho do textbox (polegadas)
-pos_x_base100 = 20     # Posição X base 100
-pos_y_base100 = 20     # Posição Y base 100
-
-pos_x = 4     # Posição X
+altura_slide = 7.5 # Altura do slide em polegadas
+pos_x = 2.65     # Posição X
 pos_y = 2     # Posição Y
-largura = 8   # Largura textbox
-altura = 2    # Altura textbox
-font_size = 32  # Tamanho da fonte
+largura_textbox = 8   # Largura textbox
+altura_textbox = 2    # Altura textbox
+font_size = 48  # Tamanho da fonte
 font_name = "BANDEX" # Fonte
+cor = [255, 255, 255]
 
 
 def selecionar_arquivo():
@@ -29,20 +25,35 @@ def selecionar_arquivo():
     )
     return arquivos_txt  # Retorna uma tupla com os caminhos dos arquivos
 
-def calcular_polegadas(pixels):
-    return pixels / 96
+def calcular_polegadas(EMU):
+    return EMU / 914400
 
-def criar_apresentacao(txt_file, pos_x, pos_y, largura, altura, font_size, font_name):
+def calcular_largura(altura):
+    """Calcula a largura com base na altura para manter a proporção 16:9."""
+    return (16 / 9) * altura
+    
+
+def criar_apresentacao(txt_file, pos_x, pos_y, largura_textbox, altura_textbox, font_size, font_name, r, g, b):
     """Cria uma apresentação PowerPoint a partir de um arquivo .txt."""
     # Define o nome do arquivo PowerPoint de saída com base no nome do arquivo de texto
     arquivo_ppt = f'{txt_file.split("/")[-1].replace(".txt", "")}.pptx'
     
     prs = Presentation()
 
+    largura_slide = calcular_largura(altura_slide)
+    # Define o tamanho da apresentação
+    prs.slide_width = Inches(largura_slide)
+    prs.slide_height = Inches(altura_slide)
+
+    
+    print("A largura e altura da apresentação é: ", prs.slide_width / 914400, prs.slide_height)
+
     # Abre o arquivo .txt e lê as linhas
     with open(txt_file, 'r', encoding='utf-8') as file:
         linhas = file.readlines()
 
+    path_img = add_imagem()
+    print("path_img: ", path_img)
 
     # Para cada linha do arquivo .txt, cria um slide e adiciona o texto
     for linha in linhas:
@@ -50,33 +61,62 @@ def criar_apresentacao(txt_file, pos_x, pos_y, largura, altura, font_size, font_
         print(linha)
         if linha:  # Só adiciona se a linha não for vazia
             slide = prs.slides.add_slide(prs.slide_layouts[5])  # Cria um slide branco e sem título
-            #adiciona o textbox:
-            textbox = slide.shapes.add_textbox(Inches(pos_x), Inches(pos_y), Inches(largura), Inches(altura))
-            text_frame = textbox.text_frame # Acessa o textbox
-            text_frame.text = linha # Insere o conteúdo da string linha no textbox
-            text_frame.auto_size = True    # Ajusta automaticamente o tamanho da caixa
-            # Define a posição central da caixa de texto
-            textbox.left = int(largura_polegadas / 2)
-            p = text_frame.paragraphs[0] # Acessa o primeiro (e único) parágrafo
-            p.font.size = Pt(font_size) # Define o tamanho da fonte
-            p.font.name = font_name # Define a fonte
 
-    # Itera sobre cada slide
-    for slide in prs.slides:
-        # Verifica se o slide tem um título
-        if slide.shapes.title:
-            # Verifica se o título está vazio
-            if not slide.shapes.title.text.strip():
-                # Remove a caixa de título se estiver vazia
-                slide.shapes._spTree.remove(slide.shapes.title._element)
+            largura_polegadas = prs.slide_width
+            altura_polegadas = prs.slide_height
+
+                        # Verifica se o slide tem um título
+            if slide.shapes.title:
+                # Verifica se o título está vazio
+                if not slide.shapes.title.text.strip():
+                    # Remove a caixa de título se estiver vazia
+                    slide.shapes._spTree.remove(slide.shapes.title._element)
+            # left = top = 0
+            # slide.shapes.add_picture(path_img, left-0.1*prs.slide_width, top, height = prs.slide_height)
+            # Adiciona a imagem ajustada ao tamanho do slide
+            slide.shapes.add_picture(path_img, 0, 0, largura_polegadas, altura_polegadas)
+
+            textbox = slide.shapes.add_textbox(Inches(pos_x), Inches(pos_y), Inches(largura_textbox), Inches(altura_textbox)) # Adiciona o textbox
+            text_frame = textbox.text_frame                 # Acessa o textbox
+            text_frame.text = linha                         # Insere o conteúdo da string linha no textbox
+            text_frame.auto_size = True                     # Ajusta automaticamente o tamanho da caixa
+            text_frame.vertical_anchor = MSO_ANCHOR.MIDDLE  # Centraliza verticalmente
+
+            p = text_frame.paragraphs[0]         # Acessa o primeiro (e único) parágrafo
+            p.font.size = Pt(font_size)          # Define o tamanho da fonte
+            p.font.name = font_name              # Define a fonte
+            p.font.color.rgb = RGBColor(r, g, b) # Define a cor
+            p.alignment = PP_ALIGN.CENTER
+
+
+
+
 
     # Salva a apresentação PowerPoint
-    prs.save(arquivo_ppt)
-    print(f'Apresentação {arquivo_ppt} criada com sucesso!')
+    try:
+        prs.save(arquivo_ppt)
+        print(f'Apresentação {arquivo_ppt} criada com sucesso!')
+    except:
+        print("Erro ao salvar o arquivo, verifique se há uma janela aberta no PowerPoint")
+
+def add_imagem():
+    janela_add_img = tk.Toplevel()
+    janela_add_img.withdraw()  # Esconde a janela principal do tkinter
+    arquivo_img = filedialog.askopenfilenames(
+        title="Selecione o arquivo .jpg ou png para fundo do slide",
+        filetypes=[
+            ("Arquivo de Imagem", "*.jpg"),
+            ("Arquivo de Imagem", "*.png")]
+    )
+    
+    if arquivo_img:
+        return arquivo_img[0]  # Retorna apenas o primeiro arquivo selecionado
+    else:
+        return None  # Caso nenhum arquivo seja selecionado
 
 
-largura_polegadas = calcular_polegadas(largura_tela)
-altura_polegadas = calcular_polegadas(altura_tela)
+r, g, b = cor
+
 
 # Seleciona os arquivos .txt
 arquivos_txt = selecionar_arquivo()
@@ -85,4 +125,4 @@ arquivos_txt = selecionar_arquivo()
 if arquivos_txt:
     
     for arquivo_txt in arquivos_txt:  # Itera sobre cada arquivo selecionado
-        criar_apresentacao(arquivo_txt, pos_x, pos_y, largura, altura, font_size, font_name)
+        criar_apresentacao(arquivo_txt, pos_x, pos_y, largura_textbox, altura_textbox, font_size, font_name, r, g, b)
